94ae8e24131a7d17e07bf5cfa1cf78f89cf4a203
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-07-30 21:43:20 +0300

    Create userTagController.js

---
+++ controllers/userTagController.js
@@ -0,0 +1,18 @@
+
+
+class userTagController {
+
+    async post(req, res) {
+
+    }
+
+    async delete(req, res) {
+
+    }
+
+    async get(req, res) {
+
+    }
+}
+
+module.exports = new userTagController()
