c991471c47abf8b5d0fb410eeea8ab1b95e776de
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-07-31 19:33:04 +0300

    Update userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,12 +1,10 @@
 const Router = require('express')
-const validationMiddlware = require('../middlware/validationMiddlware')
-const { checkSchema } = require('express-validator')
 const userController = require('../controllers/userController')
 const authMiddlware = require('../middlware/authMiddlware')
 const router = new Router()
 
 router.get('', authMiddlware, userController.get)
-router.put('', authMiddlware, checkSchema(validationMiddlware), userController.put)
+router.put('', authMiddlware ,userController.put)
 router.delete('', authMiddlware, userController.delete)
 
 module.exports = router
